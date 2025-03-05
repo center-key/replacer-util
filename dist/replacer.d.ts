2be1eda0d43cecff0860b94fb3871f78296828d7
dpilafian <dem@centerkey.com>
2025-03-04 17:52:25 -0800

    Release v1.3.3

--- dist/replacer.d.ts
+++ dist/replacer.d.ts
@@ -1,4 +1,4 @@
-//! replacer-util v1.3.2 ~~ https://github.com/center-key/replacer-util ~~ MIT License
+//! replacer-util v1.3.3 ~~ https://github.com/center-key/replacer-util ~~ MIT License
 
 export type Settings = {
     cd: string | null;
--- spec/fixtures/target/mock1.js
+++ spec/fixtures/target/mock1.js
@@ -5,7 +5,7 @@ let π1 = 3.14;
 let τ1 = 2 * π1;
 
 const info1 = {
-   banner:      '🔍🔍🔍 replacer-util v1.3.2 🔍🔍🔍',
+   banner:      '🔍🔍🔍 replacer-util v1.3.3 🔍🔍🔍',
    description: 'Find and replace strings or template outputs in text files (CLI tool designed for use in npm package.json scripts)',
    list1:       'A.I. module, A.I. module, A.I. module',
    list2:       'A.I. module, iNsEcT, INSECT, A.I. module',
