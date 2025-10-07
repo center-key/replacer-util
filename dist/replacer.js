2445e9c1809881c35c3593940f2a4b90e5fec555
pilafmon <pilaf@centerkey.com>
2025-10-07 02:53:48 -0700

    Release v1.4.1

--- dist/replacer.js
+++ dist/replacer.js
@@ -1,4 +1,4 @@
-//! replacer-util v1.4.0 ~~ https://github.com/center-key/replacer-util ~~ MIT License
+//! replacer-util v1.4.1 ~~ https://github.com/center-key/replacer-util ~~ MIT License
 
 import { EOL } from 'node:os';
 import { globSync } from 'glob';
