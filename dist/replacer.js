e422f5702123eb3900ef2de2bf3b9938ef7f13b1
pilafmon <pilaf@centerkey.com>
2025-11-17 14:45:43 -0800

    Release v1.5.1

--- dist/replacer.js
+++ dist/replacer.js
@@ -1,4 +1,4 @@
-//! replacer-util v1.5.0 ~~ https://github.com/center-key/replacer-util ~~ MIT License
+//! replacer-util v1.5.1 ~~ https://github.com/center-key/replacer-util ~~ MIT License
 
 import { cliArgvUtil } from 'cli-argv-util';
 import { EOL } from 'node:os';
--- spec/target/bundle.js
+++ spec/target/bundle.js
@@ -6,7 +6,7 @@ let π1 = 3.14;
 let τ1 = 2 * π1;
 
 const info1 = {
-   banner:      '🔍🔍🔍 replacer-util v1.5.0 🔍🔍🔍',
+   banner:      '🔍🔍🔍 replacer-util v1.5.1 🔍🔍🔍',
    description: 'Find and replace strings or template outputs in text files (CLI tool designed for use in npm package.json scripts)',
    list1:       'insect, insect, insect',
    list2:       'insect, iNsEcT, INSECT, insect',
@@ -23,7 +23,7 @@ let π2 = 3.14;
 let τ2 = 2 * π2;
 
 const info2 = {
-   banner:      '🔍🔍🔍 replacer-util v1.5.0 🔍🔍🔍',
+   banner:      '🔍🔍🔍 replacer-util v1.5.1 🔍🔍🔍',
    description: 'Find and replace strings or template outputs in text files (CLI tool designed for use in npm package.json scripts)',
    code:        'mock2',
    file:        'spec/fixtures/web/subfolder-a/mock2.js',
